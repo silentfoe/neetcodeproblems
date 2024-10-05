2df74be7121684effa86503da6cb4e7b802992ba
Spencer Pingitore <silentfoe6@gmail.com>
2024-10-05 17:15:06 -0400

    adding comments and better solution than I came up with initially. Learning about hash sets

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,7 +1,36 @@
+# Duplicate Integer
+# Solved 
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Example 1:
+
+# Input: nums = [1, 2, 3, 3]
+
+# Output: true
+# Example 2:
+
+# Input: nums = [1, 2, 3, 4]
+
+# Output: false
+
+#My solution using brute force
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         for num in nums:
             if nums.count(num) > 1:
                 return True
         
+        return False
+    
+
+
+#better solution using hash set:
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
         return False
