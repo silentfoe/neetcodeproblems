7ed14de9ff460728f930291fd8deb8d285313942
Spencer Pingitore <silentfoe6@gmail.com>
2024-10-05 17:02:14 -0400

    adding my solution to this problem

---
+++ contains_duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for num in nums:
+            if nums.count(num) > 1:
+                return True
+        
+        return False
